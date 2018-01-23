7490bea357d716e0e00c5c61ce46c5740805320e
Jeppe Ernst <jeppe@colourbox.com>
2018-01-23 10:19:26 +0100

    all tests passing

--- week01/myFractions.py
+++ week01/myFractions.py
@@ -2,8 +2,15 @@
 
 class MyFraction:
 
-    def __init__(self, fractionString):
-        self.numerator, self.denominator = [ int(x) for x in fractionString.split("/") ]
+    def __init__(self, numerator, denominator = 1):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    @classmethod
+    def fromString(cls, fractionString):
+        numerator, denominator = [ int(x) for x in fractionString.split("/") ]
+        #print("num: {0} den: {1} input:{2}".format(numerator, denominator, fractionString))
+        return cls(numerator, denominator)
 
     def getWholeNumber(self):
         return int(self.numerator / self.denominator)
--- week01/testFraction.py
+++ week01/testFraction.py
@@ -4,15 +4,16 @@ from myFractions import MyFraction as Fraction
 
 class TestFractions(unittest.TestCase):
 
-    def test_isEqual(self):
-        fraction = Fraction("3/3")
-        print(fraction.getWholeNumber())
+    def test_fractionFromString(self):
+        fraction = Fraction.fromString("2/2")
         self.assertEqual(1, fraction.getWholeNumber())
 
-    def test_isNotEqual(self):
-        self.assertNotEqual(1, 2)
+    def test_isEqual(self):
+        fraction = Fraction(3, 3)
+        self.assertEqual(1, fraction.getWholeNumber())
+
 
     def test_addTwoFractions(self):
-        frac1 = Fraction("2/4")
-        frac2 = Fraction("1/2")
+        frac1 = Fraction(2, 4)
+        frac2 = Fraction(1, 2)
         self.assertEqual(1, frac1.add(frac2))
