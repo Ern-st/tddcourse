4366e2cdb45e382d5d27ab2f560c001ba0fe4d0e
Jeppe Ernst <jeppe@colourbox.com>
2018-02-09 09:10:44 +0100

    regenerated Slack webhook and moved to env variable

--- week03/theRealMcCoy.py
+++ week03/theRealMcCoy.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import os
+
 from MyStringAdder  import MyStringAdder
 from readerClass    import reader
 from readerClass    import webReader
@@ -9,7 +11,7 @@ from loggerClass    import slackLogger
 MyWebStringAdder = MyStringAdder(
     webReader({"url":"https://gist.githubusercontent.com/ernst-at-colourbox/3197dbb764785c309ea3af84d8a6727f/raw/c47d0cf76289cc54ad0dd437309f24752095f993/TDD_week03_input.txt"}), 
     writer(), 
-    slackLogger({"webhook":" https://hooks.slack.com/services/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"})
+    slackLogger({"webhook": os.environ['SLACK_WEBHOOK_ME']})
 )
 
 MyStringAdder = MyStringAdder(
